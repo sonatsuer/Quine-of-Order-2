77a4858fedb236b8ea099c28e7b95edd68c48c49
Sonat Süer <sonatsuer@gmail.com>
2015-06-10 13:40:44 +0300

    First and probably the last commit.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Quine-of-Order-2
+
+These are two Haskell files meant to be nerdy gifts.
+When executed, sonat.hs produces the file tulay.hs and
+tulay.hs produces the file sonat.hs.
+
+There is nothing special about Haskell. The idea can be
+implemented in pretty much any language.
---
+++ sonat.hs
@@ -0,0 +1,36 @@
+-- sonat.hs
+
+import System.IO
+import Data.List
+import Data.Char
+
+main = do
+     writeFile output text
+     putStrLn (['L','o','a','d',' ','a','n','d',' ','r','u','n',' '] ++ output)
+
+pieces = ["-- sonat.hs",
+          "-- tulay.hs",
+          "",
+          "import System.IO",
+          "import Data.List",
+          "import Data.Char",
+          "",
+          "main = do",
+          "     writeFile output text",
+          "     putStrLn (['L','o','a','d',' ','a','n','d',' ','r','u','n',' '] ++ output)",
+          "",
+          "pieces = [",
+          "",
+          "output = drop 3 (pieces !! 1)",
+          "",
+          "text =  lines' (drop 1 (take 12 pieces)) ++ (punctuate . swapFirstTwo) pieces ++ [']', chr 10] ++ lines' (drop 12 pieces)",
+          "               where lines' = intercalate [chr 10]",
+          "                     swapFirstTwo (x0:x1:xs) = x1:x0:xs",
+          "                     punctuate = intercalate ([',',chr 10] ++ replicate 10 ' ') . map (([chr 34] ++).( ++ [chr 34]))"]
+
+output = drop 3 (pieces !! 1)
+
+text =  lines' (drop 1 (take 12 pieces)) ++ (punctuate . swapFirstTwo) pieces ++ [']', chr 10] ++ lines' (drop 12 pieces)
+               where lines' = intercalate [chr 10]
+                     swapFirstTwo (x0:x1:xs) = x1:x0:xs
+                     punctuate = intercalate ([',',chr 10] ++ replicate 10 ' ') . map (([chr 34] ++).( ++ [chr 34]))
